a33408d15f91de7ae05ced72fc036485d964c199
Christoffer Lundqvist <chribbelundqvist@gmail.com>
2019-01-15 13:25:22 +0100

    1

--- src/hogwartsprojekt/HogwartsProjekt.java
+++ src/hogwartsprojekt/HogwartsProjekt.java
@@ -12,7 +12,7 @@ public class HogwartsProjekt {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // TODO code application logic here,sks
     }
     
 }
